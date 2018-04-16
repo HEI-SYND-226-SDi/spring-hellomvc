6234573b10a867632af20bf6c12b8238d1619555
cm0x4d <cm0x4d@codemonkey.ch>
2018-04-16 17:17:52 +0200

    Created project using start.spring.io

---
+++ src/main/java/ch/hevs/sdi/hellomvc/HelloMvcApplication.java
@@ -0,0 +1,12 @@
+package ch.hevs.sdi.hellomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMvcApplication.class, args);
+    }
+}
